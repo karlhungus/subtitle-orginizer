61cfc718d4a154c64944aea4317d27ba9a6f8afe
Ben Noel <imperator.kujira+git@gmail.com>
2011-03-28 20:08:31 +0900

    cleanup, example script now works on Windows

--- src/organizer/head.py
+++ src/organizer/head.py
@@ -1,35 +1,12 @@
 from subtitleOrganizer import subtitleOrganizer 
-from criterion import *
 
+#currently only windows compatible
 subRepository = 't:\\subtitles\\'
-#subRepository = '/Users/xamn/pythonplay/SubRep/'
 movieShack = 't:\\Movieshack\\'
-#movieShack =  '/Users/xamn/pythonplay/Movieshack/'
+
 
 
 a = subtitleOrganizer(subRepository,movieShack)
+a.organize()
 
-for i in a.organize():
-    print(i)
-    
-
-
-
-
-"""
-a = TvSeasonEpisodeCriterion()
-
-b = '23x11'
-c = '23x11'
-
-print(a.score('23x11','23x11')) #m
-print(a.score('s23e11','s23e11')) #m
-print(a.score('23X11','23x11')) #m
-print(a.score('S23E11','s23e11')) #m
-print(a.score('23x11','S23E11')) #m
-print(a.score('22x11','23x11')) #kill
-print(a.score('23x10','23x11')) #kill
-print(a.score('hello','23x11')) #kill
-print(a.score('23x11','hello')) #kill
-"""
 
